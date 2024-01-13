f3fa61fb14769cc06737ee68da8a67852051aa13
Asimmons8228 <96853510+Asimmons8228@users.noreply.github.com>
2024-01-13 01:05:48 -0500

    Featured Media Commit

--- pages/home.js
+++ pages/home.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import MainLayout from '@/components/layouts/MainLayout'
+import FeaturedMedia from '@/components/UI/FeaturedMedia/FeaturedMedia'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -9,9 +10,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function CreateUser() {
   return (
     <MainLayout>
-        <div>
-            Home
-        </div>
+        <FeaturedMedia />
     </MainLayout>
   )
 }
