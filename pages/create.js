2ed23165ae7c3161b9b1b599fe0b3fcce600f368
Asimmons8228 <96853510+Asimmons8228@users.noreply.github.com>
2024-01-14 16:59:16 -0500

    Save User Commit

--- pages/create.js
+++ pages/create.js
@@ -2,13 +2,47 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useStateContext } from '@/components/HBOprovider'
+import ls from 'local-storage'
+import {v4} from 'uuid'
+import { useRouter } from 'next/router'
 
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function CreateUser() {
   const globalState = useStateContext();
-  console.log(globalState)
+  const router = useRouter();
+  const saveUser = () => {
+    let users = [],
+    user;
+
+    if(ls('users') < 1){
+      user= {
+        id: v4(),
+        user: globalState.user,
+        myListId: []
+      }
+      users.push(user)
+      ls('users', users)
+
+      console.log('users:', users)
+      console.log('lsusers', ls('users'))
+    } else {
+      users = ls('users')
+      user = {
+        id: v4(),
+        user: globalState.user,
+        myListId: []
+      }
+      users.push(user)
+      ls('users', users)
+
+      console.log('users:', users)
+      console.log('lsusers', ls('users'))
+      router.push('/login')
+    }
+  }
+
   return (
     <div>
       <div className='create-user'>
@@ -34,7 +68,7 @@ export default function CreateUser() {
         </div>
         <div className='create-user__buttons'>
           <button className='create-user__cancel'>Cancel</button>
-          <button className='create-user__save'>Save</button>
+          <button className='create-user__save' onClick={saveUser}>Save</button>
         </div>
       </div>
     </div>
