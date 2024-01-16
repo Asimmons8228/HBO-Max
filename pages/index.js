b3abe017d2ed0d64ba6ab01237b71de2b5d6f838
Asimmons8228 <96853510+Asimmons8228@users.noreply.github.com>
2024-01-16 16:34:34 -0500

    Dynamic Home Page Commit

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,58 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { useEffect } from 'react'
-import { Inter } from 'next/font/google'
-import { useStateContext } from '@/components/HBOprovider'
-import Login from '@/components/UI/Login/Login'
-import { useRouter } from 'next/router'
-import MainLayout from '@/components/layouts/MainLayout'
-import FeaturedMedia from '@/components/UI/FeaturedMedia/FeaturedMedia'
-import AuthCheck from '@/components/AuthCheck'
-import MediaRow from '@/components/UI/MediaRow/MediaRow'
+import Head from "next/head";
+import Image from "next/image";
+import { useEffect } from "react";
+import { Inter } from "next/font/google";
+import { useStateContext } from "@/components/HBOprovider";
+import Login from "@/components/UI/Login/Login";
+import { useRouter } from "next/router";
+import MainLayout from "@/components/layouts/MainLayout";
+import FeaturedMedia from "@/components/UI/FeaturedMedia/FeaturedMedia";
+import AuthCheck from "@/components/AuthCheck";
+import MediaRow from "@/components/UI/MediaRow/MediaRow";
 
-
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Index() {
   const globalState = useStateContext();
   const router = useRouter();
-  useEffect (() => {
-
-  }, [])
+  useEffect(() => {}, []);
 
   return AuthCheck(
     <>
-    <MainLayout>
+      <MainLayout>
         <FeaturedMedia />
-        <MediaRow title="Movies" type="large-v"/>
-        <MediaRow title="Series" type="small-h" />
-        <MediaRow title="Action" type="small-v" />
-        <MediaRow title="Horror" type="large-h" />
-        <MediaRow title="SciFi" type="small-v" />
-    </MainLayout>
+        <MediaRow
+          title="Movies"
+          type="large-v"
+          endpoint="discover/movie?sort_by=popularity.desc&primary_release_year=2023"
+        />
+        <MediaRow
+          title="Series"
+          type="small-h"
+          endpoint="discover/tv?sort_by=popularity.desc"
+        />
+        <MediaRow
+          title="Action"
+          type="small-v"
+          endpoint="discover/movie?with_genres=28&primary_release_year=2023"
+        />
+        <MediaRow
+          title="Horror"
+          type="large-h"
+          endpoint="discover/movie?with_genres=27&primary_release_year=2023"
+        />
+        <MediaRow
+          title="SciFi"
+          type="small-v"
+          endpoint="discover/movie?with_genres=878&primary_release_year=2023"
+        />
+        <MediaRow
+          title="Animations"
+          type="small-v"
+          endpoint="discover/movie?with_genres=16&primary_release_year=2023"
+        />
+      </MainLayout>
+      
     </>
-  )
+  );
 }
